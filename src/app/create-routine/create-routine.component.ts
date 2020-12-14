8331045daec980838b1013260fe7f9afae5ea466
AkmElias <akmelias11@gmail.com>
2020-12-15 01:03:49 +0600

    ist step of create routine ui added

--- src/app/create-routine/create-routine.component.ts
+++ src/app/create-routine/create-routine.component.ts
@@ -1,107 +1,139 @@
-import { RoutineapiService } from '../routineapi.service';
-import { Routine } from '../routine';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CourseOfferedapiService } from '../course-offeredapi.service';
-import { RoomapiService } from '../roomapi.service';
-import { ExamapiService } from '../examapi.service';
-import { TimeSlotapiService } from '../time-slotapi.service';
+import { ProgramapiService } from "./../programapi.service";
+import { RoutineapiService } from "../routineapi.service";
+import { Routine } from "../routine";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { CourseOfferedapiService } from "../course-offeredapi.service";
+import { RoomapiService } from "../roomapi.service";
+import { TimeSlotapiService } from "../time-slotapi.service";
 
 @Component({
-  selector: 'app-create-routine',
-  templateUrl: './create-routine.component.html',
-  styleUrls: ['./create-routine.component.css'],
-  providers: [RoutineapiService]
+  selector: "app-create-routine",
+  templateUrl: "./create-routine.component.html",
+  styleUrls: ["./create-routine.component.css"],
 })
 export class CreateRoutineComponent {
-
   id: number;
-  routines = [{routineID: ''}];
+  Term: String;
+  Year: Number;
+  Years = [];
+  Day: String;
+  Days = [];
+  Program: String;
+  programs = [];
+  firstStep = false;
+  routines = [{ routineID: "" }];
   Routine;
   coursesOffered: any = [];
   rooms: any = [];
   exams: any = [];
   timeSlots: any = [];
-
   submitted = false;
 
   constructor(
-    private routineService: RoutineapiService,  private api: CourseOfferedapiService,
-    private roomService: RoomapiService, private examService: ExamapiService,
-    private timeslotService: TimeSlotapiService, private router: Router)
-
-  {
-    this.Routine = {routineID: '', courseOfferedID: '', roomCode: '', examID: '', timeSlotID: '' };
+    private routineService: RoutineapiService,
+    private programApi: ProgramapiService,
+    private api: CourseOfferedapiService,
+    private roomService: RoomapiService,
+    private timeslotService: TimeSlotapiService,
+    private router: Router
+  ) {
+    this.Routine = {
+      routineID: "",
+      courseOfferedID: "",
+      roomCode: "",
+      examID: "",
+      timeSlotID: "",
+    };
+    this.getPrograms();
+    this.Term = "";
+    this.Year = 2020;
+    this.Years = [2020, 2021, 2022, 2023, 2024, 2025];
+    this.Day = "";
+    this.Days = [
+      "Saturday",
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+    ];
+    this.Program = "";
     this.getCoursesOffered();
     this.getRooms();
-    this.getExams();
     this.getTimeSlots();
   }
 
+  getPrograms = () => {
+    this.programApi.getAllPrograms().subscribe((data) => {
+      this.programs = data;
+      console.log("programs: ", data);
+    });
+  };
+
+  programSelected = () => {
+    if (this.Program) {
+      this.firstStep = true;
+    }
+  };
+
+  addTermYearDayProgram = () => {
+    alert(`${this.Program} ${this.Day} ${this.Term} ${this.Year} `);
+  };
+
   getCoursesOffered = () => {
     this.api.getAllCoursesOffered().subscribe(
-      data => {
+      (data) => {
         this.coursesOffered = data;
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
-  }
+  };
 
   getRooms = () => {
     this.roomService.getAllRooms().subscribe(
-      data => {
+      (data) => {
         this.rooms = data;
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
-  }
-
-  getExams = () => {
-    this.examService.getAllExams().subscribe(
-      data => {
-        this.exams = data;
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+  };
 
   getTimeSlots = () => {
     this.timeslotService.getAllTimeSlots().subscribe(
-      data => {
+      (data) => {
         this.timeSlots = data;
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
-  }
+  };
 
   createRoutine = () => {
     this.routineService.createRoutine(this.Routine).subscribe(
-      data => {
+      (data) => {
         this.routines.push(data);
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
 
     this.submitted = false;
     this.gotoList();
-  }
+  };
 
-    onSubmit() {
+  onSubmit() {
     this.submitted = true;
     this.createRoutine();
   }
 
   gotoList() {
-    this.router.navigate(['/routine']);
+    this.router.navigate(["/routine"]);
   }
 }
