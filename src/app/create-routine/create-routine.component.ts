2b4ddb6afc37ef18738e7cd99f0f9c22e56f73b7
AkmElias <akmelias11@gmail.com>
2020-12-18 16:58:53 +0600

    normal routine addition done ... filtering and validation checking and dp post remains

--- src/app/create-routine/create-routine.component.ts
+++ src/app/create-routine/create-routine.component.ts
@@ -33,13 +33,16 @@ export class CreateRoutineComponent {
   batches = [];
   Section: Number;
   sections = [];
+  batchAndSectionId = {};
   Time: String;
   timeSlots = [];
   Course: Number;
   offeredCourses = [];
   availableCourses = [];
   courses = [];
-  Faculty: String;
+  Faculty: Number;
+  courseShortName: String;
+  facultyShortName: String;
   Room: String;
   batchAndSection: String;
   courseAndFaculty: String;
@@ -50,15 +53,20 @@ export class CreateRoutineComponent {
   roomLoaded = false;
   courseLoaded = false;
   secondStepLoaded = false;
+  facultyIds = [];
+  routines = [];
+  idRoutines = [];
 
-  routines = [{ routineID: "" }];
   Routine;
   coursesOffered: any = [];
+  availableCourse = [];
+
   selectedCourseOffered: Number;
   rooms: any = [];
   exams: any = [];
   dayRoutineByProgram = [];
   routineRow = {};
+  idRoutineRow = {};
   submitted = false;
 
   constructor(
@@ -83,10 +91,11 @@ export class CreateRoutineComponent {
       timeSlotID: "",
     };
     this.getPrograms();
-    this.Term = "";
+    this.Term = "Spring";
     this.Year = 2020;
     this.Years = [2020, 2021, 2022, 2023, 2024, 2025];
-    this.Day = "";
+    this.Day = "SATURDAY";
+    this.Program = "CSE";
     this.Days = [
       "SATURDAY",
       "SUNDAY",
@@ -98,6 +107,7 @@ export class CreateRoutineComponent {
     this.Program = "";
     this.Batch = 0;
     this.Section = 0;
+    this.batchAndSectionId = {};
     this.Time = "";
     this.timeSlots = [
       "8:00am to 9:20am",
@@ -107,6 +117,8 @@ export class CreateRoutineComponent {
       "2:00pm to 3:20pm",
       "3:30pm to 4:50pm",
     ];
+    this.Faculty = null;
+    this.Course = null;
     console.log("time slots...", this.timeSlots);
     this.routineRow = {
       title: "",
@@ -117,6 +129,24 @@ export class CreateRoutineComponent {
       room: "",
     };
     this.selectedCourseOffered = 0;
+    this.courseShortName = "";
+    this.facultyShortName = "";
+    this.routineRow = {
+      title: "",
+      batchAndSection: "",
+      timeSlot: "",
+      course: "",
+      faculty: "",
+      room: "",
+    };
+    this.idRoutineRow = {
+      batch: "",
+      section: "",
+      timeSlot: "",
+      course: "",
+      faculty: "",
+      room: "",
+    };
     this.getCoursesOffered();
     this.getRooms();
     this.getTimeSlots();
@@ -125,14 +155,14 @@ export class CreateRoutineComponent {
   getPrograms = () => {
     this.programApi.getAllPrograms().subscribe((data) => {
       this.programs = data;
-      console.log("programs: ", data);
+      // console.log("programs: ", data);
     });
   };
 
   programSelected = () => {
-    if (this.Program) {
+    if (this.Program && this.Term && this.Year && this.Program) {
       this.firstStep = true;
-      console.log("Program Code..", this.Program);
+      // console.log("Program Code..", this.Program);
     }
   };
 
@@ -140,7 +170,7 @@ export class CreateRoutineComponent {
     this.firstStepSubmitted = true;
     this.batchApi.getBatchesByProgram(this.Program).subscribe((data) => {
       this.batches = data;
-      console.log("batches..", data);
+      // console.log("batches..", data);
     });
 
     this.programs.filter((program) => {
@@ -153,59 +183,112 @@ export class CreateRoutineComponent {
 
   batchSelected = (batchId) => {
     this.batchLoaded = true;
+    this.sectionLoaded = false;
+    this.timeSlotLoaded = false;
+    this.courseLoaded = false;
+    this.roomLoaded = false;
+    this.secondStepLoaded = false;
+    this.Section = null;
+    this.sections = [];
+    this.offeredCourses = [];
+    this.batches.filter((batch) => {
+      if (batch.id == this.Batch) {
+        this.batchAndSection = batch.batchName + " ";
+      }
+    });
     this.sectionApi.getSectionsByBatch(batchId).subscribe((data) => {
       console.log("sections..", data);
       this.sections = data;
+      this.Section = null;
     });
+    // this.Section = null;
   };
 
-  sectionSelected = () => {
+  sectionSelected = async () => {
     this.sectionLoaded = true;
-    let batchAndSection = {
+    this.timeSlotLoaded = false;
+    this.courseLoaded = false;
+    this.roomLoaded = false;
+    this.secondStepLoaded = false;
+    this.Course = null;
+    this.courses = [];
+    this.offeredCourses = [];
+    this.batchAndSectionId = {
       batch: this.Batch,
       section: this.Section,
     };
-    this.courseOfferedApi
-      .getAllCourseOfferedToBatchAndSection(batchAndSection)
+    this.sections.filter((section) => {
+      if (section.id == this.Section) {
+        this.batchAndSection += section.sectionName;
+      }
+    });
+    console.log("batch and section..", this.batchAndSection);
+    await this.courseOfferedApi
+      .getAllCourseOfferedToBatchAndSection(this.batchAndSectionId)
       .subscribe((data) => {
         this.offeredCourses = data;
+        this.Course = null;
+        this.offeredCourses.forEach(async (offer) => {
+          let tempCourse = {
+            faculty: offer.facultyID,
+            courseId: offer.courseID,
+            course: "",
+          };
+          await this.courseApi
+            .getOneCourse(offer.courseID)
+            .subscribe((data) => {
+              tempCourse.course = data[0].crs_title;
+              this.availableCourse.push(tempCourse);
+            });
+        });
+        this.Course = null;
+        this.Time = "";
+
         console.log("offered courses..", this.offeredCourses);
       });
   };
 
   timeSlotSelected = () => {
     this.timeSlotLoaded = true;
+    this.courseLoaded = false;
+    this.roomLoaded = false;
+    this.secondStepLoaded = false;
+    this.Course = null;
+    this.Room = null;
+    this.rooms = [];
     let asignFaculty = [];
-
+    this.batchAndSectionId = {
+      batch: this.Batch,
+      section: this.Section,
+    };
+    console.log(
+      "offered courses with crs title and facultyid..",
+      this.availableCourse
+    );
     this.availableCourses = this.courses.filter((course) => {});
   };
 
   courseSelected = async (courseOffered) => {
     this.courseLoaded = true;
-    if (
-      this.batchLoaded == true &&
-      this.sectionLoaded == true &&
-      this.timeSlotLoaded == true &&
-      this.courseLoaded == true &&
-      this.roomLoaded == true
-    ) {
-      this.secondStepLoaded = true;
-    }
+    this.roomLoaded = false;
+    this.secondStepLoaded = false;
+    this.Room = null;
+    this.rooms = [];
     let cfids = [];
-    let courseId;
-    this.offeredCourses.filter((ofcr) => {
-      if (courseOffered == ofcr.id) {
-        return (courseId = ofcr.courseID);
+
+    this.availableCourse.filter((ofcr) => {
+      if (courseOffered == ofcr.courseId) {
+        return (this.Course = ofcr.courseId);
       }
     });
-    let facultyId;
-    this.offeredCourses.filter((ofcr) => {
-      if (courseOffered == ofcr.id) {
-        return (facultyId = ofcr.facultyID);
+
+    this.availableCourse.filter((ofcr) => {
+      if (courseOffered == ofcr.courseId) {
+        return (this.Faculty = ofcr.faculty);
       }
     });
-    cfids.push(courseId);
-    cfids.push(facultyId);
+    cfids.push(this.Course);
+    cfids.push(this.Faculty);
 
     let i = 0;
     let promises = cfids.map((id) => {
@@ -219,10 +302,14 @@ export class CreateRoutineComponent {
       }
     });
 
+    await this.roomApi.getAllRooms().subscribe((data) => {
+      this.rooms = data;
+    });
     let results = await Promise.all(promises);
     let courseAndFacultyShortName =
       results[0][0].crs_shortName + ", " + results[1][0].fac_shortName;
-
+    this.courseShortName = results[0][0].crs_shortName;
+    this.facultyShortName = results[1][0].fac_shortName;
     console.log("course and fac shortname..", courseAndFacultyShortName);
   };
 
@@ -239,7 +326,7 @@ export class CreateRoutineComponent {
     ) {
       this.secondStepLoaded = true;
     }
-    console.log("loded fields..", this.secondStepLoaded);
+    // console.log("loded fields..", this.secondStepLoaded);
   };
 
   getCoursesOffered = () => {
@@ -273,6 +360,29 @@ export class CreateRoutineComponent {
         console.log(error);
       }
     );
+  };
+
+  addRoutineRaw = () => {
+    this.routineRow = {
+      title: this.RoutineTitle,
+      batchAndSection: this.batchAndSection,
+      timeSlot: this.Time,
+      course: this.courseShortName,
+      faculty: this.facultyShortName,
+      room: this.Room,
+    };
+    this.idRoutineRow = {
+      batch: this.Batch,
+      section: this.Section,
+      timeSlot: this.Time,
+      course: this.Course,
+      faculty: this.Faculty,
+      room: this.Room,
+    };
+    console.log("day routine..", this.routineRow);
+    console.log("day routine in ids..", this.idRoutineRow);
+    this.routines.push(this.routineRow);
+    this.idRoutines.push(this.idRoutineRow);
   };
 
   createRoutine = () => {
